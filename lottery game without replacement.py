3a8c2a4feb67b8a18b45b8bcc69695567fe54514
Sumit Kumar Pandey <pandeyaniket223344@gmail.com>
2025-08-02 23:13:00 +0545

    First commit

---
+++ lottery game without replacement.py
@@ -0,0 +1,16 @@
+import random
+lottery_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+draw_count = int(input("Enter how many numbers you want to draw: "))
+
+print("Lottery Draw (Without Replacement):")
+for i in range(draw_count):
+    if len(lottery_numbers) == 0:
+        print("No more numbers left to draw!")
+        break
+
+    picked = random.choice(lottery_numbers)
+    print("Number", i+1, ":", picked)
+  
+    lottery_numbers.remove(picked)
